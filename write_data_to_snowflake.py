b349bb1f5d3f6f812a5ae9628471611336c2d418
Salvador Vidal <salvador.vidalm97@gmail.com>
2021-03-16 15:30:01 -0600

    Upload data to Snowflake

--- write_data_to_snowflake.py
+++ write_data_to_snowflake.py
@@ -1,8 +1,39 @@
-from pyspark.sql import SparkSession
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from py4j.java_gateway import java_import
 
+## @params: [JOB_NAME, URL, ACCOUNT, WAREHOUSE, DB, SCHEMA, USERNAME, PASSWORD]
+SNOWFLAKE_SOURCE_NAME = "net.snowflake.spark.snowflake"
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'URL', 'ACCOUNT', 'WAREHOUSE', 'DB', 'SCHEMA', 'USERNAME', 'PASSWORD'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+java_import(spark._jvm, SNOWFLAKE_SOURCE_NAME)
+## uj = sc._jvm.net.snowflake.spark.snowflake
+spark._jvm.net.snowflake.spark.snowflake.SnowflakeConnectorUtils.enablePushdownSession(spark._jvm.org.apache.spark.sql.SparkSession.builder().getOrCreate())
+sfOptions = {
+"sfURL" : args['URL'],
+"sfAccount" : args['ACCOUNT'],
+"sfUser" : args['USERNAME'],
+"sfPassword" : args['PASSWORD'],
+"sfDatabase" : args['DB'],
+"sfSchema" : args['SCHEMA'],
+"sfWarehouse" : args['WAREHOUSE'],
+}
 
-if __name__ == '__main__':
-    spark = SparkSession \
-        .builder \
-        .appName('Pokemon') \
-        .getOrCreate()
+## Read from a Snowflake table into a Spark Data Frame
+# df = spark.read.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable", "POKEMON_DATA").load()
+s3_path = 's3://bucket/prefix/*.parquet'
+poke_df = spark.read.parquet(s3_path)
+#poke_df.show()
+
+## Perform any kind of transformations on your data and save as a new Data Frame: df1 = df.[Insert any filter, transformation, or other operation]
+## Write the Data Frame contents back to Snowflake in a new table 
+poke_df.write.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable", "[tableName]").mode("overwrite").save()
+# job.commit()
