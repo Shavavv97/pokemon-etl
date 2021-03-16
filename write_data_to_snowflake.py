c723cc47f83a01d8f0a1406910bf8364b9f0267d
Salvador Vidal <salvador.vidalm97@gmail.com>
2021-03-16 15:36:22 -0600

    Upload data to Snowflake

--- write_data_to_snowflake.py
+++ write_data_to_snowflake.py
@@ -15,7 +15,6 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 java_import(spark._jvm, SNOWFLAKE_SOURCE_NAME)
-## uj = sc._jvm.net.snowflake.spark.snowflake
 spark._jvm.net.snowflake.spark.snowflake.SnowflakeConnectorUtils.enablePushdownSession(spark._jvm.org.apache.spark.sql.SparkSession.builder().getOrCreate())
 sfOptions = {
 "sfURL" : args['URL'],
@@ -27,13 +26,7 @@ sfOptions = {
 "sfWarehouse" : args['WAREHOUSE'],
 }
 
-## Read from a Snowflake table into a Spark Data Frame
-# df = spark.read.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable", "POKEMON_DATA").load()
-s3_path = 's3://bucket/prefix/*.parquet'
-poke_df = spark.read.parquet(s3_path)
-#poke_df.show()
-
-## Perform any kind of transformations on your data and save as a new Data Frame: df1 = df.[Insert any filter, transformation, or other operation]
-## Write the Data Frame contents back to Snowflake in a new table 
-poke_df.write.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable", "[tableName]").mode("overwrite").save()
-# job.commit()
+if __name__ == '__main__':
+    s3_path = 's3://bucket/prefix/*.parquet'
+    poke_df = spark.read.parquet(s3_path)
+    poke_df.write.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable", "[tableName]").mode("overwrite").save()
